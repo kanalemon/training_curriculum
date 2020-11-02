95df6af0a79a1b375f08ad35024c951a99bb9049
kanalemon <kanalemon@icloud.com>
2020-11-02 18:19:18 +0900

    Revert "Merge branch 'master' into second_training"
    This reverts commit ddf7cdf0ebb39e6e815369ee5b591638539d0864, reversing
    changes made to 351d7e5d561cc2da57ab0ca114b1a5170466eaf7.

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -35,7 +35,6 @@ class CalendarsController < ApplicationController
       plan = plans.map do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-      
       wday_num = Date.today.wday + x
       #もしもwday_numが7以上であれば、7を引く
       if wday_num >= 7
@@ -44,5 +43,6 @@ class CalendarsController < ApplicationController
       days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans, wday: wdays[wday_num]}
       @week_days.push(days)
     end
+
   end
 end
